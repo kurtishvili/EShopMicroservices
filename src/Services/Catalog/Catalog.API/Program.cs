f9721ceb0d4fd08351316cd70935dbce55bbdded
Gio Kurtishvili <kurtishvilii@gmail.com>
2024-12-06 21:43:40 +0400

    add building blocks and add docker orchestration

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var services = builder.Services;
+
+services.AddCarter();
+services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var connectionString = builder.Configuration.GetConnectionString("Database");
+
+services.AddMarten(options =>
+{
+    options.Connection(connectionString!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
 // Configure the Http request pipeline
+
+app.MapCarter();
 
 app.Run();
