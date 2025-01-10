08983bebd34a895fd9cc79d9242e732a5aa19d42
Giorgi Kurtishvili <78432652+kurtishvili@users.noreply.github.com>
2025-01-10 18:01:55 +0400

    impmenet ordering domain layer

---
+++ src/Services/Ordering/Ordering.Domain/Models/ValueObjects/OrderItemId.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.Models.ValueObjects;
+
+public record OrderItemId
+{
+    public Guid Value { get; }
+
+    private OrderItemId(Guid value) => Value = value;
+
+    public static OrderItemId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+            throw new DomainException("Order item id cannot be empty");
+
+        return new OrderItemId(value);
+    }
+}
